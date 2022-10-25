92de1893d92e98a14fab35c7077fc61ef14241f4
Mingkun Ma <mingkunm@amazon.com>
2022-10-25 14:40:19 -0700

    Add basic mocks
    Signed-off-by: Mingkun Ma <mingkunm@amazon.com>

---
+++ common/index.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const PLUGIN_ID = 'searchRelevance';
+export const PLUGIN_NAME = 'Search Relevance';
+
+export const pageStyles: CSS.Properties = {
+  float: 'left',
+  width: '100%',
+  maxWidth: '1130px',
+};
+
+export interface IDocType {
+  [key: string]: string;
+}
