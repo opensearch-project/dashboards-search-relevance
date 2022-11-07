1531dfc747b47379bb6e36dab74a2f744a819d7b
Mingkun Ma <mingkunm@amazon.com>
2022-11-07 10:56:24 -0800

    Add babelrc config
    Signed-off-by: Mingkun Ma <mingkunm@amazon.com>

--- public/components/common_utils/__tests__/utils.test.ts
+++ public/components/common_utils/__tests__/utils.test.ts
@@ -3,10 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// TODO: Update unit test
-const isNameValid = (name) => {
-  return name.length >= 50 || name.length === 0 ? false : true;
-};
+import { isNameValid } from '../utils';
 
 describe('Utils helper functions', () => {
   it('validates isNameValid function', () => {
