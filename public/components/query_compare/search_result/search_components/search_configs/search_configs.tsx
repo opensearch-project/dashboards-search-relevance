d715233e2be1cb6f796fdea2de9a7d46e253cbf6
Nicholas Ung <nicholasung22@gmail.com>
2023-10-05 18:32:56 -0700

    Update horizontal and vertical rules to comply with
    Signed-off-by: Nicholas Ung <nicholasung22@gmail.com>

--- public/components/query_compare/search_result/search_components/search_configs/search_configs.tsx
+++ public/components/query_compare/search_result/search_components/search_configs/search_configs.tsx
@@ -44,11 +44,10 @@ export const SearchConfigsPanel = ({
   return (
     <EuiPanel
       hasShadow={false}
-      hasBorder={false}
       color="transparent"
       grow={false}
       borderRadius="none"
-      style={{ borderBottom: '1px solid #D3DAE6' }}
+      className="right-border left-border"
     >
       <EuiFlexGroup>
         <EuiFlexItem className="search-relevance-config">
