8d4328e167bffcf72a0aa06c2ccacf39ee48888d
sumukhswamy <sumukhhs@amazon.com>
2024-10-21 15:37:39 -0700

    [Bug] changed path for error handling for apis
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- server/routes/dsl_route.ts
+++ server/routes/dsl_route.ts
@@ -195,7 +195,7 @@ export function registerDslRoute(router: IRouter,  openSearchServiceSetup: OpenS
         );
         if (error.statusCode !== 404) console.error(error);
         return response.custom({
-          statusCode: error.statusCode || 500,
+          statusCode: error.statusCode || 400,
           body: error.message,
         });
       }
