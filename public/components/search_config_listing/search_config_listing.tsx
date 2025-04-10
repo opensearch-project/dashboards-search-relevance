3019855e1b67cdaffe95cf342e76d086f6996930
wrigleyDan <dwrigley@opensourceconnections.com>
2025-04-10 09:26:53 +0200

    search configuration table overview as component in single page app
    Signed-off-by: wrigleyDan <dwrigley@opensourceconnections.com>
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

---
+++ public/components/search_config_listing/search_config_listing.tsx
@@ -0,0 +1,96 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EuiButtonEmpty,
+} from '@elastic/eui';
+import React from 'react';
+import { TableListView, reactRouterNavigate } from '../../../../../src/plugins/opensearch_dashboards_react/public';
+import { CoreStart } from '../../../../../src/core/public';
+import { getSearchConfigurations } from '../../services';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+
+interface SearchConfigurationListingProps  extends RouteComponentProps {
+  http: CoreStart['http'];
+}
+
+export const SearchConfigurationListing: React.FC<SearchConfigurationListingProps> = ({ http, history }) => {
+  // Column definitions
+  const tableColumns = [
+    {
+      field: 'search_configuration_name',
+      name: "Name",
+      dataType: 'string',
+      sortable: true,
+      render: (
+        name: string,
+        searchConfiguration: {
+          id: string;
+        }
+      ) => (
+        <>
+          <EuiButtonEmpty
+            size="xs"
+            {...reactRouterNavigate(history, `searchConfiguration/${searchConfiguration.id}`)}
+          >
+            {name}
+          </EuiButtonEmpty>
+        </>
+      ),
+    },
+    {
+      field: 'query_body',
+      name: "Query DSL Body",
+      dataType: 'string',
+      sortable: false,
+    },
+    {
+      field: 'timestamp',
+      name: "Timestamp",
+      dataType: 'string',
+      sortable: true,
+    },
+  ];
+
+  const mapSearchConfigurationFields = (obj: any) => {
+    return {
+      id: obj.id,
+      name: obj.name,
+      description: obj.query_body,
+      timestamp: obj.timestamp,
+    };
+  };
+
+  // Data fetching function
+  const findSearchConfigurations = async (search: any) => {
+    const response = await getSearchConfigurations(http);
+    // TODO: how to report error
+    const list = response ? JSON.parse(response.resp).map(mapSearchConfigurationFields) : [];
+    // Filtering functionality could be implemented here
+    // if (search) {
+    //   list.filter((item) => item.name.includes(search));
+    // }
+    return {
+      total: list.length,
+      hits: list,
+    };
+  };
+
+  return (
+    <TableListView
+      headingId="searchConfigurationListingHeading"
+      entityName="Search Configuration"
+      entityNamePlural="Search Configurations"
+      tableListTitle="Search Configurations"
+      tableColumns={tableColumns}
+      findItems={findSearchConfigurations}
+    />
+  );
+};
+
+export const findSearchConfigurationWithRoute = withRouter(findSearchConfigurationListing);
+
+export default SearchConfigurationListingWithRoute;
