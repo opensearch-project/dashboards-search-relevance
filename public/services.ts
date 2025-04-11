ef5d9b54c9647ea1eda7b6944c88774d4e76a136
wrigleyDan <dwrigley@opensourceconnections.com>
2025-04-11 14:07:57 +0200

    adding routes, adjusting form to search config inputs
    Signed-off-by: wrigleyDan <dwrigley@opensourceconnections.com>
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

--- public/services.ts
+++ public/services.ts
@@ -49,14 +49,14 @@ export const getSearchConfigurations = async (http: any) => {
   }
 };
 
-export const postSearchConfiguration = async (name: string, description: string, sampling: string, query_set_size: number, http: any) => {
+export const postSearchConfiguration = async (name: string, query_body: string, search_pipeline: string, search_template: string, http: any) => {
   try {
     const response = await http.post(`..${BASE_SEARCH_CONFIGURATION_NODE_API_PATH}`, {
       body: JSON.stringify({
         name,
-        description,
-        sampling,
-        query_set_size,
+        query_body,
+        search_pipeline,
+        search_template,
       }),
     });
     return response;
