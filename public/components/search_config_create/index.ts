6653a3a0758b23de374ad9827bfb2c5695bbc92a
Fen Qin <mfenqin@amazon.com>
2025-04-15 16:06:09 -0700

    UI changes on experiment page and query_sets and search_configurations pages to support POST, PUT, GET, DELETE operations
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

--- public/components/search_config_create/index.ts
+++ public/components/search_config_create/index.ts
@@ -3,4 +3,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export { QuerySetListingWithRoute } from './query_set_listing';
+export { SearchConfigurationCreateWithRouter } from './search_config_create';
