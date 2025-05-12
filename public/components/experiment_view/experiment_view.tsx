27b04244f5a51e36e1b5ad6c51618c34cce5ae05
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2025-05-12 18:40:19 +0200

    Create component to view pairwise experiments in preparation for further experiment types
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

--- public/components/experiment_view/experiment_view.tsx
+++ public/components/experiment_view/experiment_view.tsx
@@ -4,33 +4,17 @@
  */
 
 import {
-    EuiButtonEmpty,
-    EuiCallOut,
     EuiPageHeader,
     EuiPageTemplate,
-    EuiPanel,
-    EuiResizableContainer,
-    EuiDescriptionList,
-    EuiDescriptionListTitle,
-    EuiDescriptionListDescription,
-    EuiSpacer,
   } from '@elastic/eui';
-import {
-    TableListView,
-    reactRouterNavigate,
-} from '../../../../../src/plugins/opensearch_dashboards_react/public';  
 import React, { useCallback, useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { CoreStart } from '../../../../../src/core/public';
 import { ServiceEndpoints } from '../../../common';
-import { VisualComparison, convertFromSearchResult } from '../query_compare/search_result/visual_comparison/visual_comparison';
 import {
-  SearchResults,
-  QueryEvaluation,
-  QuerySnapshot,
-  toQueryEvaluations,
-  toQuerySnapshots,
+  toExperiment,
 } from '../../types/index';
+import { PairwiseExperimentViewWithRouter } from './pairwise_experiment_view';
 
 interface ExperimentViewProps extends RouteComponentProps<{ id: string }> {
   http: CoreStart['http'];
@@ -38,43 +22,14 @@ interface ExperimentViewProps extends RouteComponentProps<{ id: string }> {
 
 export const ExperimentView: React.FC<ExperimentViewProps> = ({ http, id, history }) => {
   const [experiment, setExperiment] = useState<any | null>(null);
-  const [searchConfigurations, setSearchConfigurations] = useState<any | null>(null);
-  const [querySet, setQuerySet] = useState<any | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [selectedQuery, setSelectedQuery] = useState<number | null>(null);
-  const [queryResult1, setQueryResult1] = useState<SearchResults | null>(null);
-  const [queryResult2, setQueryResult2] = useState<SearchResults | null>(null);
-
-  const [queryEvaluations, setQueryEvaluations] = useState<QueryEvaluation[]>([]);
-  const [querySnapshots, setQuerySnapshots] = useState<QuerySnapshot[]>([]);
-
-  const [tableColumns, setTableColumns] = useState<any[]>([]);
-
-  const sanitizeResponse = (response) => response?.hits?.hits?.[0]?._source || undefined;
-
-  const loadSearchConfigurations = async (searchConfigurationIds: string[]) => {
-    const promises = searchConfigurationIds.map(async (id) => {
-      return await http.get(ServiceEndpoints.SearchConfigurations + "/" + id).then(sanitizeResponse);
-    });
-    return Promise.all(promises);
-  };
 
   useEffect(() => {
     const fetchExperiment = async () => {
       try {
-        setLoading(true);
-        const _rawExperiment = await http.get(ServiceEndpoints.Experiments + "/" + id)
-        const _experiment = sanitizeResponse(_rawExperiment)
-        const _searchConfigurations = _experiment ? await loadSearchConfigurations(_experiment.searchConfigurationList) : [];
-        const _querySet = _experiment ? await http.get(ServiceEndpoints.QuerySets + "/" + _experiment.querySetId).then(sanitizeResponse) : null;
-
-        if (_experiment && _searchConfigurations.length >= 2 && _searchConfigurations.every(Boolean)) {
+        const _experiment = await http.get(ServiceEndpoints.Experiments + "/" + id).then(toExperiment)
+        if (_experiment) {
           setExperiment(_experiment);
-          setSearchConfigurations(_searchConfigurations);
-          setQuerySet(_querySet);
-          setQueryEvaluations(toQueryEvaluations(_rawExperiment))
-          setQuerySnapshots([toQuerySnapshots(_rawExperiment, "0"), toQuerySnapshots(_rawExperiment, "1")])
         } else {
           setError('No matching experiment found');
         }
@@ -89,208 +44,18 @@ export const ExperimentView: React.FC<ExperimentViewProps> = ({ http, id, histor
     fetchExperiment();
   }, [http, id]);
 
-  function extractMetricNames(queryEvaluations: any): string[] {
-    if (queryEvaluations.length > 0) {
-      return Object.keys(queryEvaluations[0].metrics)
-    }
-    return [];
-  }
-
-  useEffect(() => {
-    if (experiment) {
-      const metricNames = extractMetricNames(queryEvaluations)
-
-      const cheatColNames = {
-        rbo90: "RBO@" + experiment.k,
-        jaccard: "Jaccard@" + experiment.k,
-      }
-
-      let columns = [
-        {
-            field: 'queryText',
-            name: 'Query',
-            dataType: 'string',
-            sortable: true,
-            render: (
-                name: string,
-            ) => (
-                <EuiButtonEmpty
-                    size="xs"
-                    onClick={() => {
-                      const index = queryEvaluations.findIndex(q => q.queryText === name)
-                      setSelectedQuery(index)
-                    }}
-                    title={name}
-                >
-                    {name}
-                </EuiButtonEmpty>
-              ),
-            
-        },
-      ]
-      metricNames.forEach(metricName => {
-        if (cheatColNames[metricName]) {
-          columns.push({
-            field: 'metrics.' + metricName,
-            name: cheatColNames[metricName],
-            dataType: 'number',
-            sortable: true,
-            render: (value) => {
-              if (value !== undefined && value !== null) {
-                return new Intl.NumberFormat(undefined, {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                }).format(value);
-              }
-              return '-';
-            }
-          })
-        }
-      })
-
-      setTableColumns(columns)
-      setLoading(false);
-    }
-  }, [experiment, queryEvaluations])
-
-  function resolve_attributes(ids, hits) {
-    const res = ids.map(id => hits.find(hit => hit._id === id) || {_id: id})
-    return res.map((x, index) => ({...x, rank: index + 1}))
-  }
-
-  useEffect(() => {
-    if (selectedQuery != null) {
-      const queryText = queryEvaluations[selectedQuery].queryText;
-      const snapshot1 = querySnapshots[0].find(s => s.queryText === queryText).documentIds
-      const snapshot2 = querySnapshots[1].find(s => s.queryText === queryText).documentIds
-      const query1 = {
-        index: searchConfigurations[0].index,
-        query: {
-          terms: {
-            _id: snapshot1
-          }
-        }
-      }
-      const query2 = {
-        index: searchConfigurations[1].index,
-        query: {
-          terms: {
-            _id: snapshot2
-          }
-        }
-      }
-
-      http.post(ServiceEndpoints.GetSearchResults, {
-          body: JSON.stringify({ query1, query2 }),
-      })
-      .then((res) => {
-        setQueryResult1(resolve_attributes(snapshot1, convertFromSearchResult(res.result1)))
-        setQueryResult2(resolve_attributes(snapshot2, convertFromSearchResult(res.result2)))
-      })
-      .catch((error: Error) => {
-          console.error(error);
-      });
-
-    }
-  }, [selectedQuery])
-
-  const findQueries = useCallback(async (search: any) => {
-    const filteredQueryEntries = search ?
-      queryEvaluations.filter(q => q.queryText.includes(search)) :
-      queryEvaluations;
-    return { hits: filteredQueryEntries, total: filteredQueryEntries.length };
-  }, [queryEvaluations]);
-
-  const experimentDetails = (
-    <EuiPanel hasBorder={true}>
-      <EuiDescriptionList type="column" compressed>
-        <EuiDescriptionListTitle>Query Set</EuiDescriptionListTitle>
-        <EuiDescriptionListDescription>
-          <EuiButtonEmpty
-            size="xs"
-            {...reactRouterNavigate(history, `/querySet/view/${querySet?.id}`)}
-          >
-            {querySet?.name}
-          </EuiButtonEmpty>
-        </EuiDescriptionListDescription>
-        {searchConfigurations?.map((config, index) => (
-          <React.Fragment key={config.id}>
-            <EuiDescriptionListTitle>Search Configuration {index + 1}</EuiDescriptionListTitle>
-            <EuiDescriptionListDescription>
-              <EuiButtonEmpty
-                size="xs"
-                {...reactRouterNavigate(history, `/searchConfiguration/view/${config.id}`)}
-              >
-                {config.name}
-              </EuiButtonEmpty>
-            </EuiDescriptionListDescription>
-          </React.Fragment>
-        ))}
-      </EuiDescriptionList>
-    </EuiPanel>
-  );
-
-  const resultsPane = (
-    <EuiPanel hasBorder paddingSize="l">
-      <EuiResizableContainer>
-        {(EuiResizablePanel, EuiResizableButton) => {
-          return (
-          <>
-            <EuiResizablePanel initialSize={50} minSize="15%">
-              {error ? (
-                <EuiCallOut title="Error" color="danger">
-                  <p>{error}</p>
-                </EuiCallOut>
-              ) : (
-                <TableListView
-                  key={`table-${queryEvaluations.length}`}
-                  entityName="Query"
-                  entityNamePlural="Queries"
-                  tableColumns={tableColumns}
-                  findItems={findQueries}
-                  loading={loading}
-                  pagination={{
-                    initialPageSize: 10,
-                    pageSizeOptions: [5, 10, 20, 50],
-                  }}
-                  search={{
-                    box: {
-                      incremental: true,
-                      placeholder: 'Query...',
-                      schema: true,
-                    },
-                  }}
-                />
-              )}
-            </EuiResizablePanel>
-
-            <EuiResizableButton />
-
-            <EuiResizablePanel initialSize={50} minSize="30%">
-              {selectedQuery != null && queryResult1 && queryResult2 && (
-                <VisualComparison
-                  queryResult1={queryResult1}
-                  queryResult2={queryResult2}
-                  queryText={queryEvaluations[selectedQuery].queryText}
-                  resultText1={`${searchConfigurations[0].name} result`}
-                  resultText2={`${searchConfigurations[1].name} result`}
-                />
-              )}
-            </EuiResizablePanel>
-          </>
-        )}}
-      </EuiResizableContainer>
-    </EuiPanel>
-  );
-
   return (
     <EuiPageTemplate paddingSize="l" restrictWidth="90%">
       <EuiPageHeader
         pageTitle="Experiment Visualization"
       />
-      {experimentDetails}
-      <EuiSpacer size="l" />
-      {resultsPane}
+      {experiment && experiment.type === "PAIRWISE_COMPARISON" &&
+        <PairwiseExperimentViewWithRouter
+          http={http}
+          inputExperiment={experiment}
+          history={history}
+        />
+      }
     </EuiPageTemplate>
   );
 
