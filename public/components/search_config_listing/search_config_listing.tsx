0d00c84ed946f64f93e9e8a1a60986c72fd254c5
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2025-05-22 11:29:29 +0200

    First version of sidebar navigation
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
    Signed-off-by: Fen Qin <mfenqin@amazon.com>

--- public/components/search_config_listing/search_config_listing.tsx
+++ public/components/search_config_listing/search_config_listing.tsx
@@ -5,6 +5,7 @@
 
 import {
   EuiButtonEmpty,
+  EuiButton,
   EuiButtonIcon,
   EuiCallOut,
   EuiFlexItem,
@@ -172,14 +173,16 @@ export const SearchConfigurationListing: React.FC<SearchConfigurationListingProp
         description="View and manage your existing search configurations. Click on a configuration name
                 to view details."
         rightSideItems={[
-          <EuiButtonEmpty
-            iconType="arrowLeft"
+          <EuiButton
+            onClick={() => history.push('/searchConfiguration/create')}
+            fill
             size="s"
-            onClick={() => history.push('/')}
-            data-test-subj="backToHomeButton"
+            iconType="plus"
+            data-test-subj="createSearchConfigurationButton"
+            color="primary"
           >
-            Back to Home
-          </EuiButtonEmpty>,
+            Create Search Configuration
+          </EuiButton>,
         ]}
       />
 
