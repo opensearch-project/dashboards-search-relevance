daa92752a05aeb079c36d554d044e058c1d395ef
wrigleyDan <dwrigley@opensourceconnections.com>
2025-05-30 10:01:26 +0200

    remove template from search config input form as it's currently unused, thus not working and misleading and confusing for users.
    Signed-off-by: wrigleyDan <dwrigley@opensourceconnections.com>

--- public/components/search_config_create/search_configuration_form.tsx
+++ public/components/search_config_create/search_configuration_form.tsx
@@ -145,7 +145,7 @@ export const SearchConfigurationForm: React.FC<SearchConfigurationFormProps> = (
           Search Pipeline <i> - optional </i>
         </p>
       }
-      helpText="Define the search pipeline to be used." 
+      helpText="Define the search pipeline to be used."
       fullWidth
     >
       <EuiComboBox
@@ -163,22 +163,5 @@ export const SearchConfigurationForm: React.FC<SearchConfigurationFormProps> = (
       />
     </EuiFormRow>
 
-    <EuiFormRow
-      label={
-        <p>
-          Search Template <i> - optional </i>
-        </p>
-      }
-      helpText="Define the search template."
-      fullWidth
-    >
-      <EuiFieldText
-        placeholder="Enter search template"
-        value={searchTemplate}
-        onChange={(e) => setSearchTemplate(e.target.value)}
-        fullWidth
-        disabled={disabled}
-      />
-    </EuiFormRow>
   </EuiForm>
 );
