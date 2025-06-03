d699e18eda4574b9ead9e0b129dfb4d4ae906782
Fen Qin <mfenqin@amazon.com>
2025-06-03 15:16:23 -0700

    Revert "Add DEVELOPER_GUIDE.md for router setup + fix missing files (#493)"
    This reverts commit d2b881037bb5bf8c028f7fa2026ef9514b69618c.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,13 +7,9 @@ import { IRouter, OpenSearchServiceSetup } from '../../../../src/core/server';
 import { registerDslRoute } from './dsl_route';
 import { registerMetricsRoute } from './metrics_route';
 
-export function defineRoutes(
-  router: IRouter,
-  openSearchServiceSetup: OpenSearchServiceSetup,
-  dataSourceEnabled: boolean
-) {
+export function defineRoutes(router: IRouter, openSearchServiceSetup: OpenSearchServiceSetup, dataSourceEnabled: boolean) {
   registerDslRoute(router, openSearchServiceSetup, dataSourceEnabled);
   registerMetricsRoute(router);
 }
 
-export * from './search_relevance_route_service';
+export * from './search_relevance_rourte_servce';
