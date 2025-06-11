2739558dd3bab4f0bdd915405c2f964b1d392765
alexeyrodriguez <alexey.rodriguez@gmail.com>
2025-06-11 17:55:04 +0200

    POST Query Sets endpoint was missing querySetSize in its schema validation spec. (#542)
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>

--- server/routes/search_relevance_route_service.ts
+++ server/routes/search_relevance_route_service.ts
@@ -22,6 +22,7 @@ export function registerSearchRelevanceRoutes(router: IRouter): void {
           name: schema.string(),
           description: schema.string(),
           sampling: schema.string(),
+          querySetSize: schema.number(),
         }),
       },
     },
