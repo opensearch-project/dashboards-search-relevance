1ca67d6aee2d3b095307a6231b87bb661a5d4c49
alexeyrodriguez <alexey.rodriguez@gmail.com>
2025-07-22 23:01:38 +0200

    Improve messaging when backend plugin is disabled (#578)
    * Error message when SRW backend plugin is not enabled in all listing pages.
    
     * Propagate more information in backend errors for better user reporting.
    
    
    Signed-off-by: Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>
    Co-authored-by: wrigleyDan <dwrigley@opensourceconnections.com>
    Co-authored-by: Eric Pugh <epugh@opensourceconnections.com>

--- public/components/query_set_listing/query_set_listing.tsx
+++ public/components/query_set_listing/query_set_listing.tsx
@@ -23,7 +23,7 @@ import {
   TableListView,
 } from '../../../../../src/plugins/opensearch_dashboards_react/public';
 import { CoreStart } from '../../../../../src/core/public';
-import { Routes, ServiceEndpoints } from '../../../common';
+import { extractUserMessageFromError, Routes, ServiceEndpoints } from '../../../common';
 import { DeleteModal } from '../common/DeleteModal';
 import { useConfig } from '../../contexts/date_format_context';
 
@@ -158,8 +158,9 @@ export const QuerySetListing: React.FC<QuerySetListingProps> = ({ http, history 
         hits: filteredList,
       };
     } catch (err) {
-      console.error('Failed to load query set', err);
-      setError('Failed to load query sets');
+      console.error('Failed to load query sets', err);
+      const errorMessage = extractUserMessageFromError(err);
+      setError(errorMessage ? errorMessage : 'Failed to load query sets due to an unknown error.');
       return {
         total: 0,
         hits: [],
