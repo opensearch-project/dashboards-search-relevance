2bbb30f7247254f3785f124aaee9809b4fce7fc1
Janelle Arita <janarita@amazon.com>
2026-03-19 07:45:54 -0500

    Remove milliseconds from timestamp display format (#799)
    * Refine the millisecond removal rule.  Plugin gets its date format from the parent
    ---------
    
    Signed-off-by: Janelle <janarita@amazon.com>
    Signed-off-by: Eric Pugh <epugh@opensourceconnections.com>
    Co-authored-by: Eric Pugh <epugh@opensourceconnections.com>

--- public/contexts/date_format_context.tsx
+++ public/contexts/date_format_context.tsx
@@ -18,7 +18,7 @@ interface ConfigContextProps {
 }
 
 const ConfigContext = createContext<ConfigContextProps>({
-  dateFormat: 'MMM D, YYYY @ HH:mm:ss.SSS', // Default format
+  dateFormat: 'MMM D, YYYY @ HH:mm:ss', // Default format
   dataSourceEnabled: false,
   dataSourceManagement: {} as DataSourceManagementPluginSetup,
   setHeaderActionMenu: () => {},
@@ -44,7 +44,9 @@ export const ConfigProvider: React.FC<{
   navigation,
   children,
 }) => {
-  const dateFormat = uiSettings.get('dateFormat') || 'MMM D, YYYY @ HH:mm:ss.SSS';
+  const rawDateFormat = uiSettings.get('dateFormat');
+  // UI renders to second, not millisecond level of detail so drop any millisecond format rules
+  const dateFormat = rawDateFormat.replace(/\.S{1,3}/g, '');
 
   return (
     <ConfigContext.Provider
